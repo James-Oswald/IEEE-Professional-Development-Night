90846fa70e71a9722a492d25f13f600707dfe536
James-Oswald <jamesoswald111@gmail.com>
2020-12-28 23:30:16 -0500

    added solution for problem 2

--- 12-28-20/div.py
+++ 12-28-20/div.py
@@ -1,6 +1,12 @@
 
+#https://www.codewars.com/kata/564057bc348c7200bd0000ff
 
-def thirt(num):
+def thirtMax(num):
     seq = [1, 10, 9, 12, 3, 4]
-    s = sum([int(str(num)[i]) * seq[i % 6] for i in range(len(str(num)))])
-    
+    result = sum([int(str(num)[::-1][i]) * seq[i % 6] for i in range(len(str(num)))])
+    return num if num == result else thirt(result)
+
+thirt = lambda num, res=0: num if num == (res := sum([int(str(num)[::-1][i]) * [1, 10, 9, 12, 3, 4][i % 6] for i in range(len(str(num)))])) else thirt(res)
+
+print(thirt(1234567))
+print(thirt(321))
