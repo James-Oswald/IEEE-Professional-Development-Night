22c6335b78aa29c6b0316e6c80aa293dc8a53e67
James-Oswald <jamesoswald111@gmail.com>
2021-01-18 21:53:39 -0500

    solved decode numbers

--- 1-18-21/decodeNum.py
+++ 1-18-21/decodeNum.py
@@ -1,16 +1,21 @@
 
-
-
+#This question was provided to us from one of our members
+#https://cdn.discordapp.com/attachments/795816943344680982/800891961275449364/Screen_Shot_2021-01-12_at_5.png
 
 def decodeNumbers(inp):
-    if len(inp) == 1:
+    if len(inp) <= 1:
         return 1
     acc = 0
-    if len(inp) > 2 and int(inp[0:2]) <= 26:
+    if len(inp) >= 2 and int(inp[0:2]) <= 26:
         acc += decodeNumbers(inp[2:])
-    acc += decodeNumbers(inp[1:])
+    if len(inp) >= 1 and int(inp[1]) != 0:
+        acc += decodeNumbers(inp[1:])
     return acc
 
-print(decodeNumbers("12")) #2 |  1 2 or 12
+print(decodeNumbers("12")) #2    | 1 2 or 12
 print(decodeNumbers("123")) #3   | 1 2 3 or 12 3 or 1 23
 print(decodeNumbers("127")) #2   | 1 2 7 or 12 7
+print(decodeNumbers("1028")) #1  | 10 2 8
+print(decodeNumbers("101010")) #1 
+print(decodeNumbers("2016")) #2  | 20 16 or 20 1 6 
+print(decodeNumbers("1212")) #5  | 1 2 1 2 or 12 12 or 1 21 2 or 1 2 12 or 12 1 2 
