7181a82677bbd7d29865128a0ab19ee4a9cadf2f
James-Oswald <jamesoswald111@gmail.com>
2021-03-09 12:10:34 -0500

    added js createNumber

---
+++ 3-8-21/createNumber.js
@@ -0,0 +1,7 @@
+
+
+let createNumber=n=>"("+n[0]+n[1]+n[2]+") "+n[3]+n[4]+n[5]+"-"+n[6]+n[7]+n[8]+n[9];
+//let createNumber=(n,a=)=>;
+
+console.log(createNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])) //"(123) 456-7890"
+
