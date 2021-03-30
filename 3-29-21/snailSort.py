53335a88ee17fcefc4b060089af32a18adf6cd8f
James-Oswald <jamesoswald111@gmail.com>
2021-03-30 13:08:00 -0400

    fixed snailsort

--- 3-29-21/snailSort.py
+++ 3-29-21/snailSort.py
@@ -3,25 +3,25 @@
 
 def snailSort(matrix):
     n = len(matrix)
-    visited = [[0] * n] * n
+    visited = [[0]*n for i in range(n)]
     directionsX = [1, 0, -1, 0]
-    directionsY = [0, -1, 0, 1]
+    directionsY = [0, 1, 0, -1]
     directionIndex = 0
     x = 0
     y = 0
     outputArray = []
     canMove = True
     while canMove:
-        outputArray.append(matrix[x][y])
-        visited[x][y] = 1
+        outputArray.append(matrix[y][x])
+        visited[y][x] = 1
         nextX = x + directionsX[directionIndex]
         nextY = y + directionsY[directionIndex]
-        if nextX >= n or nextY >= n or visited[nextX][nextY] == 1:
+        if nextX >= n or nextY >= n or visited[nextY][nextX] == 1:
             directionIndex += 1
-            
+            directionIndex %= 4
             nextX = x + directionsX[directionIndex]
             nextY = y + directionsY[directionIndex]
-        if nextX >= n or nextY >= n or visited[nextX][nextY] == 1:
+        if nextX >= n or nextY >= n or visited[nextY][nextX] == 1:
             canMove = False
         x = nextX
         y = nextY
